104b625a3166f6b38c06f48b86d0e41ba0e2d141
tr-c <tc82@rice.edu>
2023-11-18 14:15:34 -0600

    last sprint

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//library to help make HTTP Requests 
+import axios from "axios"
+import {useState, useEffect} from 'react'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const App = () => {
+	//"Endpoint" URL where the data is located
+	const URL = "https://collectionapi.metmuseum.org/public/collection/v1/objects/895906"
+  // rand id: 895906
+
+	// Set up the state to store the fetched data
+	const [data, setData] = useState([])
+
+	useEffect(() => {
+		const fetchData = async () => {
+			console.log("Making GET Request");
+			try {
+				// GET Request
+				const response = await axios.get(URL)
+						 .then((response) => {
+						//console.log(response)
+            setData(response.data)
+				})
+			} catch (error) {
+				// Handle any errors
+        console.error('Failed to fetch data: ', error);
+			}
+		}
+
+		fetchData()
+    const year = data["accessionYear"]
+    const art_title = data["title"]
+    const artist_name = data["artistDisplayName"]
+    console.log(art_title + " by " + artist_name + ", " + year)
+	}, [data]); // remove data to stop infinite loop
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
